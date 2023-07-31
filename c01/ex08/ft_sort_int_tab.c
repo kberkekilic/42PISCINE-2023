335c01b92c5b4b31c3776dd857defd65d05b8abd
Kadir Berke Kılıç <kberkekilic@outlook.com>
2023-07-31 14:43:29 +0300

    c01

---
+++ c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kadkilic <kadkilic@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/05 18:37:40 by kadkilic          #+#    #+#             */
+/*   Updated: 2023/04/06 02:17:56 by kadkilic         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	tmp;
+
+	j = 0;
+	while (j < size)
+	{
+		i = j + 1;
+		while (i < size)
+		{
+			if (tab[i] >= tab[j])
+			{
+				tmp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = tmp;
+			}
+			i++;
+		}
+		i++;
+	}
+}
