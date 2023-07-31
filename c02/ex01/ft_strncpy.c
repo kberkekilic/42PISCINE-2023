3799cc93bcc753274c445477e11671944d7e8c52
Kadir Berke Kılıç <kberkekilic@outlook.com>
2023-07-31 14:43:12 +0300

    c02

---
+++ c02/ex01/ft_strncpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kadkilic <kadkilic@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/10 20:01:08 by kadkilic          #+#    #+#             */
+/*   Updated: 2023/04/10 20:01:13 by kadkilic         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
